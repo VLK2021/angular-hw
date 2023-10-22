d1d2901a15fb2ee38d64bef6f3a5a523b690c892
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-10-22 22:19:49 +0300

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import { CounterComponent } from './app-components/counter/counter.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
   ],
   imports: [
     BrowserModule,
