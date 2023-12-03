fd7442a8d53024ff223a09180a419bb3ee09e940
vlk2022 <volodymyrkostiuk2021@gmail.com>
2023-12-03 02:17:24 +0200

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 import {AppComponent} from './app.component';
+import { LocationComponent } from './components/location/location.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LocationComponent,
   ],
   imports: [
     BrowserModule,
